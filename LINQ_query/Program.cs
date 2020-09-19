72af5c2371f110b6e609a5eb0ec9ce0cacb74056
Rijan Chapagain <hrijon.chp@gmail.com>
2020-09-19 12:46:04 +0800

    Adds data source and creates query

--- LINQ_query/Program.cs
+++ LINQ_query/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,13 @@ namespace LINQ_query
     {
         static void Main(string[] args)
         {
+            // Get data source
+            string[] language = {"Communication", "Collaboration", "Love", "Peace",
+                                    "Complementry", "Compassionate", "Calm", "Courteous", "Supportive", "Motivative", "Helpful"};
+            // Create query
+            var positiveWords = from lang in language
+                         where lang.Contains('C')
+                         select lang;
         }
     }
 }
